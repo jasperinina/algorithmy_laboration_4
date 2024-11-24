37243ea7ffcabc87481af7131394d361e2d89db2
AetbaevaJulia <aetbaevajulia05@gmail.com>
2024-11-25 02:09:34 +0500

    add UI elements for task 3

--- Lab4/Lab4/Task_3/Task_3Page.xaml.cs
+++ Lab4/Lab4/Task_3/Task_3Page.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
 
 namespace Lab4.Task_3;
 
@@ -6,11 +7,76 @@ public partial class Task_3Page : Page
 {
     private MainWindow _mainWindow;
     private StackPanel dynamicPanel;
-    
+
     public Task_3Page(MainWindow mainWindow)
     {
         InitializeComponent();
         _mainWindow = mainWindow;
         InitializeComponent();
+    }
+
+    private static double[,] E()
+    {
+        int[] sizeText = { 100, 500, 1000, 2000, 5000, 7000, 10000, 15000, 20000 };
+        double[,] result = new double[sizeText.Length, 3];
+        for (int i = 0; i < sizeText.Length; i++)
+        {
+            string text = TextHandler.GenerateText(sizeText[i]);
+            Stopwatch stopwatchQ = Stopwatch.StartNew();
+            QuickSortForString q = new QuickSortForString(text);
+            stopwatchQ.Stop();
+            Stopwatch stopwatchR = Stopwatch.StartNew();
+            RadixSort r = new RadixSort(text);
+            stopwatchR.Stop();
+
+            result[i, 0] = sizeText[i];
+            result[i, 1] = stopwatchQ.Elapsed.TotalMilliseconds;
+            result[i, 2] = stopwatchR.Elapsed.TotalMilliseconds;
+        }
+
+
+        return result;
+    }
+
+    // Строка для таблицы
+    public class DataItem
+    {
+        public string WordCount { get; set; }
+        public string QuickSort { get; set; }
+        public string RadixSort { get; set; }
+    }
+
+    // Замеры времени для таблицы
+    private void btForTest_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        double[,] data = E();
+        List<DataItem> items = new List<DataItem>();
+        for (int i = 0; i < data.GetLength(0); i++)
+        {
+            items.Add(new DataItem
+            {
+                WordCount = data[i, 0].ToString(),
+                QuickSort = data[i, 1].ToString(),
+                RadixSort = data[i, 2].ToString()
+            });
+        }
+        DataListView.ItemsSource = items;
+    }
+
+    // Подсчет слов для готового текста
+    private void btForCountWordsForDoc_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        string path = tBoxForLengthWords.Text;
+        TextHandler TH = new TextHandler(path);
+        string text = TH.Text;
+        tBoxForCountWords.Text = TextHandler.CountWord(text.Split(" "));
+    }
+
+    // Кнопка для подсчета слов для сгенерированного текста
+    private void btForCountWordsForGeneratedText_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        int inputLengthText = Convert.ToInt32(tBoxForLengthWords.Text);
+        string text = TextHandler.GenerateText(inputLengthText);
+        tBoxForCountWords.Text = TextHandler.CountWord(text.Split(" "));
     }
 }
--- Lab4/Lab4/Task_3/TextHandler.cs
+++ Lab4/Lab4/Task_3/TextHandler.cs
@@ -13,13 +13,13 @@ namespace Lab4.Task_3
         private static Random _random = new Random();
 
         // Принимает длину текста (количество слов)
-        public TextHandler(int length)  
-        { 
+        public TextHandler(int length)
+        {
             Text = GenerateText(length);
         }
 
         // Для считывания текста с файла, аргумент - путь к файлу
-        public TextHandler(string path) 
+        public TextHandler(string path)
         {
             string strings = string.Empty;
             StreamReader reader = new StreamReader(path, Encoding.ASCII);
@@ -31,26 +31,26 @@ namespace Lab4.Task_3
         private void ParseText(string text)
         {
             string result = new string(text.Where(t => char.IsAsciiLetter(t) || t == ' ').ToArray());
-            Text = result;
+            Text = result.ToLower();
         }
 
         // генерирует текст, принимая его длину
-        public static string GenerateText(int countOfWords) 
+        public static string GenerateText(int countOfWords)
         {
             string alf = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"; // набор букв, из которого будут состоять слова текста
             StringBuilder result = new StringBuilder();
             for (int i = 1; i <= countOfWords; i++)
             {
-                int lengthWords = _random.Next(1, 15); 
+                int lengthWords = _random.Next(1, 15);
                 for (int j = 1; j <= lengthWords; j++)
                 {
-                    int indexLetter = _random.Next(0, alf.Length - 1);  
-                    result.AppendFormat(Convert.ToString(alf[indexLetter]));  
+                    int indexLetter = _random.Next(0, alf.Length - 1);
+                    result.AppendFormat(Convert.ToString(alf[indexLetter]));
                 }
                 if (i != countOfWords)
                 {
                     result.AppendFormat(" ");
-                }                
+                }
             }
             return Convert.ToString(result);
         }
@@ -73,11 +73,11 @@ namespace Lab4.Task_3
 
             string result = string.Empty;
             foreach (var word in words)
-            { 
+            {
                 result += string.Format("{0}: {1} \n", word.Key, word.Value);
             }
 
-            return result;
+            return result[..(result.Length - 2)];
         }
     }
 }
